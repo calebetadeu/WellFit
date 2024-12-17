b79e48fed58e503fccf0b760f39d4353f1e694b8
calebetadeu@gmail.com <calebetadeu@gmail.com>
2024-12-17 12:03:38 -0300

    Adicionando as depedencias do projeto

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,11 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.room)
+
+
 }
 
 android {
@@ -38,6 +43,9 @@ android {
         compose = true
     }
 }
+room {
+    schemaDirectory("$projectDir/schemas")
+}
 
 dependencies {
 
@@ -49,6 +57,10 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.bundles.compose)
+    debugImplementation(libs.bundles.compose.debug)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -56,4 +68,19 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation(libs.androidx.credentials.core)
+    implementation(libs.androidx.credentials.compat)
+
+    coreLibraryDesugaring(libs.desugar.jdk.libs)
+
+    implementation(libs.bundles.koin)
+
+    implementation(libs.bundles.ktor)
+    implementation(libs.room.runtime)
+    implementation(libs.room.ktx)
+    ksp(libs.room.compiler)
+
+    implementation(libs.kotlinx.serialization.json)
+    testImplementation(libs.junit)
 }
